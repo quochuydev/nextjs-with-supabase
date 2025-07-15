283bb144bf50f765a84542304b07004708f767af
quochuydev <quochuy.dev@gmail.com>
2025-07-15 16:24:35 +0700

    feat: update ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,8 @@
+import { AuthButton } from "@/components/auth-button";
 import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
-import { AuthButton } from "@/components/auth-button";
-import { Hero } from "@/components/hero";
 import { GenerateImage } from "@/components/generate-image";
 import { ThemeSwitcher } from "@/components/theme-switcher";
-import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
-import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
